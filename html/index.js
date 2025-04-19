e8ffd00826e8592dece407954dac0b56b0eb5813
JyanMan <196519339+JyanMan@users.noreply.github.com.>
2025-04-19 10:40:41 +0800

    working fetch in the frontend

--- html/index.js
+++ html/index.js
@@ -1,15 +1,38 @@
 async function getData() {
-    const url = "http://localhost:5154/api/weather/weather-forecast";
+    const location = document.getElementById("location").value;
+    if (!location || location === '') {
+        throw new Error("please input a location");
+    }
+    console.log(location);
+    const url = `http://localhost:5154/api/weather/weather-forecast/current-conditions?location=${location}`;
+
     try {
         const response = await fetch(url);
         if (!response.ok) 
             throw new Error(`Response status ${response.status}`);
 
-        const jsonContent = await response.json;
-        console.log(jsonContent);
+        const jsonContent = await response.json();
+
+        if (jsonContent) {
+            UpdateCurrConditions(jsonContent);
+        }
+
     } catch (error) {
         console.error(error.message);
     }
 }
 
-getData();
+function UpdateCurrConditions(currentConditions) {
+    const conditionLabel = document.getElementById("condition");
+    const humidityLabel = document.getElementById("humidity");
+    const feelslikeLabel = document.getElementById("feelslike");
+    const temperatureLabel = document.getElementById("temperature");
+    const pressureLabel = document.getElementById("pressure");
+
+    console.log(currentConditions);
+    conditionLabel.innerText = currentConditions.conditions;
+    humidityLabel.innerText = currentConditions.humidity;
+    feelslikeLabel.innerText = currentConditions.feelslike;
+    pressureLabel.innerText = currentConditions.pressure;
+    temperatureLabel.innerText = currentConditions.temp;
+}
